98bb8c7bf3d32fcc67e171b3d1510fa7232023cc
hayatul11 <hayatulemon340@gmail.com>
2021-07-03 10:48:27 +0600

    ...

---
+++ 1. Javascript-basic/typeconversion.js
@@ -0,0 +1,44 @@
+
+// construction list 
+String
+Number
+Boolean
+
+
+var str = '1000';
+var n = 10;
+
+console.log(str * n);
+console.log(str + n);
+
+console.log(Number(str));
+console.log(Number.parseInt(str));
+
+console.log(n.toString())
+
+console.log(Number(Infinity)) // not possible
+console.log(String(Infinity)) // possible
+console.log(Boolean(Infinity)) // possible
+
+// Falsy value
+''
+0
+null
+undefined
+NaN
+
+console.log("\n");
+
+console.log(Boolean(''));
+console.log(Boolean('dsfsdaf'));
+
+console.log(Boolean(null));
+console.log(Boolean(undefined));
+
+console.log(Boolean(0));
+console.log(Boolean(456));
+
+console.log(true);
+var x = true;
+console.log(x.toString())
+
