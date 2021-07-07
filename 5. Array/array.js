7a8080bac32f13ee6eadf4f691032ae7139c305c
hayatul11 <hayatulemon340@gmail.com>
2021-07-07 21:53:43 +0600

    array basic

---
+++ 5. Array/array.js
@@ -0,0 +1,19 @@
+
+// Literal Array
+var arr = [1, 2, 3, 4, 5] 
+arr[5] = 6; //index add
+arr[10] = 11;
+
+arr[2] = 22; // date update
+
+console.log(arr)
+console.log(arr[0]) // index
+console.log(arr[7]) // undefined index
+
+// array length
+console.log(arr.length)
+
+// Contractor Array
+var arr2 = Array()
+var arr2 = Array(1, 2, 3, 4, 5);
+console.log(arr2)
