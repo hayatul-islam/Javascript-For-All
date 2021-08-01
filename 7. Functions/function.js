7d2a2ab29434a3d44ec6982f1f5b81d0208f2ad9
hayatul11 <hayatulemon340@gmail.com>
2021-08-01 17:23:49 +0600

    js functions

---
+++ 7. Functions/function.js
@@ -0,0 +1,28 @@
+
+function functionName(){
+    console.log("This is Function")
+}
+
+function add(){
+    var a = 10
+    var b = 20
+    console.log(a+b)
+}
+
+function sub(){
+    var a = 10
+    var b = 20
+    console.log(a-b)
+}
+
+functionName()
+functionName()
+for(var i=0; i<10; i++){
+    functionName()
+}
+
+add()
+add()
+
+sub()
+sub()
