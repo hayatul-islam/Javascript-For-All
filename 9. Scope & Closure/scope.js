fed5040a71f57c81ab1ff14bafef0afde13fcbf6
hayatul11 <hayatulemon340@gmail.com>
2021-08-05 17:49:13 +0600

    scope chain

--- 9. Scope & Closure/scope.js
+++ 9. Scope & Closure/scope.js
@@ -27,3 +27,32 @@
 
     scope()
 }
+
+// testing scope 
+var a = 11
+
+function A() {
+    var b = 12
+    function B() {
+        var c = 23
+        console.log(c)
+    }
+
+    function C() {
+        var d = 56
+        console.log(d)
+    }
+    console.log(b)
+    D()
+    B()
+    C()
+}
+function D(n) {
+    return n + a
+}
+
+// scope chain
+// A >> a,b,c,d B(), C(), D()
+// B >> a,b,c B(), C(), D()
+// C >> a,b,d B(), C(), D()
+// D >> a, n, A()
